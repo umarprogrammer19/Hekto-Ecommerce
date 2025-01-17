277a4ba0f815b9f59a9ff2539940afeef3005eae
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-17 23:43:57 +0500

    Schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+import Pschema from './schema';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [Pschema],
 }
