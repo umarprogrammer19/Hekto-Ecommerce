1c3feff13616aa7d5ae58529c3d4e962631ac8de
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 14:15:09 +0500

    Project Name Changes

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: "ohv29asc",
+    projectId: "muz30eox",
     dataset: "production",
     apiVersion: "2024-01-01",
     useCdn: false,
